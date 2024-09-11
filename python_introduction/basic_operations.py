042057f9e444c504e257b6400eb7b2b3f211050b
yahya yirmak <yahyayirmak9@gmail.com>
2024-09-11 07:59:18 +0100

    commit python_introduction directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+Addition = number1 + number2
+Substraction = number1 - number2
+Multiplication = number1 * number2
+print('Addition of', number1, 'and', number2, 'is', Addition)
+print('Substraction of', number1, 'and', number2, 'is', Substraction)
+print('Multiplication of', number1, 'and', number2, 'is', Multiplication)
