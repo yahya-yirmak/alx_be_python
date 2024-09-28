663ca113580b028be27e614bc0a48e1e96c07cc6
yahya yirmak <yahyayirmak9@gmail.com>
2024-09-28 16:36:21 +0100

    Modify the arithmetic_operations.py python script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,15 @@
 
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num2 != 0:
-                return num1 / num2
-            else:
-                return "You can not divide by zero!"
-        case _ :
-            return "Operation Not Found."
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "You can not divide by zero!"
+    else:
+        return "Operation Not Found."
