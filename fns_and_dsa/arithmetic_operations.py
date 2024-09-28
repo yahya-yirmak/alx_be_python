1662e7c9e72d85813603a2b53e6f78fcd8931644
yahya yirmak <yahyayirmak9@gmail.com>
2024-09-28 16:39:54 +0100

    Modify the arithmetic_operations.py python script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return num1 * num2
     elif operation == 'divide':
-        if num2 != 0:
+        if num2 == 0:
             return "You can not divide by zero!"
         else:
             return num1 / num2
