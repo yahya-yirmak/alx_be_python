59a905cb12457920821c7f9d98ffd871c66a19ce
yahya yirmak <yahyayirmak9@gmail.com>
2024-09-28 19:00:11 +0100

    Modify the shopping_list_manager.py python script

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -10,24 +10,28 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = int(input("Enter your choice: "))
-
+        try:
+            choice = int(input("Enter your choice: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+            
         if choice == 1:
-            name = input('Enter your name: ')
+            name = input('Enter the item to add: ')
             if name in shopping_list:
-                print(f'{name} is already exists.')
+                print(f'{name} already exists.')
             else:
                 shopping_list.append(name)
 
         elif choice == 2:
-            name = input('Enter your name: ')
+            name = input('Enter the item to remove: ')
             if name in shopping_list:
                 shopping_list.remove(name)
             else:
                 print(f'{name} does not exist.')
         
         elif choice == 3:
-            print(shopping_list)
+            print(f"Shopping list: {shopping_list}")
 
         elif choice == 4:
             print("Goodbye!")
