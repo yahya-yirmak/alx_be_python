d9b3fd26e628876ba0a799d4502bc708f70f40cd
yahya yirmak <yahyayirmak9@gmail.com>
2024-10-09 12:29:32 +0100

    Updating library_system.py script

--- oop/library_system.py
+++ oop/library_system.py
@@ -32,12 +32,13 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            if isinstance(book, Book):
-                print(f"Book: {book.title} by {book.author}")
+            if isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             elif isinstance(book, EBook):
                 print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
+    
 
 
 
